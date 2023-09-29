e37162b4c9997b3f73b03f0bdabedc7ee072a9f8
Tiago Moreira <tiago.joke@yahoo.com.br>
2023-09-29 19:38:42 -0300

    feat: implements metod AdicionarVeiculo

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -17,6 +17,8 @@ namespace DesafioFundamentos.Models
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string novaPlaca = Console.ReadLine();
+            veiculos.add(novaPlaca);
         }
 
         public void RemoverVeiculo()
